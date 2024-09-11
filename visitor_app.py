3a81ab5b5da8cb3dd9f3b8f90aec0d4aef6b3d9b
Elojotecno <elojotecno@gmail.com>
2024-09-11 16:37:49 +0200

    Update visitor_app.py

--- visitor_app.py
+++ visitor_app.py
@@ -163,7 +163,7 @@ def show_analytics(df, container):
         visitor_count = df.shape[0]
         col1.metric(label="Visiteurs", value=visitor_count, delta=None, help=None, label_visibility="visible")
 
-        dept_count = df['dept'].unique().count()
+        dept_count = df['dept'].unique().value_counts()
         st.write(dept_count)
         col2.metric(label="Départements", value=dept_count, delta=None, help=None, label_visibility="visible")
     
