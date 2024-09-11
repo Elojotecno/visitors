ef7215761280d2cf1e4bd4206eb2481e3b947b58
Elojotecno <elojotecno@gmail.com>
2024-09-11 18:45:03 +0200

    Update visitor_app.py

--- visitor_app.py
+++ visitor_app.py
@@ -226,9 +226,7 @@ def show_analytics(df, container):
     
         fig2 = px.histogram(df, x="date", title='Visiteurs par date & heure')
         col200.plotly_chart(fig2, use_container_width=True)
-    
-def get_zip_value():
-        st.write(search_city(st.session_state['Code postal']))    
+ 
     
 def main():
         
@@ -273,9 +271,9 @@ def main():
             farm = content.text_input('Elevage')
             name = content.text_input('Nom')
             address = content.text_input('Adresse')
-            zip = content.text_input('Code postal', on_change=get_zip_value, key='Code postal')
+            zip = content.text_input('Code postal')
             dept = zip[:2]
-            city = content.text_input('Ville')
+            city = content.multiselect("Ville", search_city(zip))
             mobile = content.text_input('Mobile')
             cows = content.text_input('Nb vaches laitières')
             milking_eqt = content.multiselect("Equipement actuel", eqt_list)
