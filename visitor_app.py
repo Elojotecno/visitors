317433f8ef6b51e1a7d0d26e53e7e6ab548ff5b9
Elojotecno <elojotecno@gmail.com>
2024-09-11 16:49:36 +0200

    Update visitor_app.py

--- visitor_app.py
+++ visitor_app.py
@@ -169,7 +169,7 @@ def show_analytics(df, container):
         dept_sales = df['sales'].nunique()
         col2.metric(label="Donateurs", value=dept_sales, delta=None, help=None, label_visibility="visible")
 
-        fig = px.pie(df, values="product", names="product", hole=.4, title='Types de')
+        fig = px.pie(df, values="product", names="product", hole=.4, title='Types de projet')
         fig.update_traces(textposition='outside', textinfo='percent+label')
         col1.plotly_chart(fig, use_container_width=True)
     
