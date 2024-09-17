2f90bd543c195089f09f2ecd68613b85cae18b9d
Elojotecno <elojotecno@gmail.com>
2024-09-17 16:17:08 +0200

    Add files via upload

--- visitor_app.py
+++ visitor_app.py
@@ -375,8 +375,8 @@ def main():
 
                 columns = st.columns(2)
                 link = data_dir + f
-                columns[0].write(link)
-                columns[1].download_button(label="Télécharger le fichier", data=pd.read_csv(link, sep=";"), file_name=f, mime="text/csv")
+                columns[index].write(link)
+                columns[index+1].download_button(label="Télécharger le fichier", data=pd.read_csv(link, sep=";"), file_name=f, mime="text/csv")
                 index += 2
     
 if __name__ == "__main__":
