18474e3e9ca3256c5d97ef56a6d8d5dccb89f19e
Elojotecno <elojotecno@gmail.com>
2024-09-17 12:47:36 +0200

    Add files via upload

--- visitor_app.py
+++ visitor_app.py
@@ -69,7 +69,7 @@ def check_password(controller):
 
     # Return True if the username + password is validated.
     if st.session_state.get("password_correct", False):
-        return True
+        return False
 
     # Show inputs for username + password.
     login_form()
